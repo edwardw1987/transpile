fe7ffd4dcf0c8514ead1d788764b2acc7c35f543
edward <edwardw@qq.com>
2018-05-06 15:51:59 +0800

    ok

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .transpile import Transpile, File, Method, translate
+from .config import _Settings
--- config.py
+++ config.py
@@ -3,3 +3,9 @@ import os
 curdir = os.path.dirname(__file__)
 
 templates_dir = os.path.join(curdir, "templates")
+
+class _Settings(dict):
+    __setattr__ = dict.__setitem__
+    __getattr__ = dict.__getitem__
+
+settings = _Settings()
