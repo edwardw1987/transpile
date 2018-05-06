d40ee8d68cb67c38d06cc2eb91d84b13650db12f
edward <edwardw@qq.com>
2018-05-06 16:03:54 +0800

    tmp

--- transpile.py
+++ transpile.py
@@ -7,6 +7,7 @@ import keyword
 class InvalidKeyword(Exception):
     pass
 
+
 class Method(object):
 
     @property
@@ -20,55 +21,60 @@ class Method(object):
     def lines(self):
         return [trans.get_result() for trans in self.get_translator()]
 
-def translate(s, indent=0, verb=False, raw=False, callable=False, args=None, kwargs=None):
-    offset = indent * chr(32)
+
+def translate(s, verb=False, raw=False, callable=False, args=None, kwargs=None):
     t = Translator(s)
     if raw is True:
-        t.set_result(offset + s, quote=True)
+        t.set_result(s, quote=True)
     elif verb is True:
-        t.set_result(offset + s)
+        t.set_result(s)
     elif callable is True:
         """ callable """
         t.p_args = args
         t.k_args = kwargs
-        t.offset = offset
     else:
         if not keyword.iskeyword(s):
             raise InvalidKeyword(s)
-        t.set_result(offset + s)
+        t.set_result(s)
     return t
+
 
 class Translator(object):
     def __init__(self, s=None):
         self.s = s
         self._quoted = self.quote(s)
-        self._text = None
+        self._result = None
         self._joins = []
         self.p_args = None
         self.k_args = None
         self.offset = ""
 
-    def has_indent(self, text):
-        return text[:4] == chr(32) * 4
-
+    def indent(self, width=4):
+        if self._result is None:
+            self.offset = chr(32) * width
+        else:
+            self._result = chr(32) * width + self._result
+        return self
+        
     def get_result(self):
         self._translate()
-        text_spans = [self._text] + [j.get_result() for j in self._joins]
+        text_spans = [self._result] + [j.get_result() for j in self._joins]
         return " ".join(text_spans)
 
     def set_result(self, text, quote=False):
-        self._text = self.quote(text) if quote else text
+        self._result = self.quote(text) if quote else text
 
     def join(self, *args, **kwargs):
         self._joins.append(translate(*args, **kwargs))
         return self
 
     def _translate(self):
-        if self._text is None and \
-            not keyword.iskeyword(self.s):
-                apd = ["%s" % self.quote(p) for p in self.p_args or []]
-                apd += ["%s=%s" % (k, self.quote(v)) for k, v in (self.k_args or {}).items()]
-                self._text = self.offset + "%s(%s)" % (self.s, ', '.join(apd))
+        if self._result is None and \
+                not keyword.iskeyword(self.s):
+            apd = ["%s" % self.quote(p) for p in self.p_args or []]
+            apd += ["%s=%s" % (k, self.quote(v))
+                    for k, v in (self.k_args or {}).items()]
+            self._result = self.offset + "%s(%s)" % (self.s, ', '.join(apd))
         return self
 
     def quote(self, s):
@@ -86,6 +92,7 @@ class Translator(object):
     #         return fn
     #     return getattr(self, attr)
 
+
 class Transpile(object):
 
     def __init__(self):
@@ -100,7 +107,7 @@ class Transpile(object):
 
 class File(object):
     def __init__(self, content):
-        self.content =  content
+        self.content = content
 
     def save(self, savefp):
         with open(savefp, "w") as outf:
